83332102d9654d56cdb881545095ac39a6e95200
Stefan Baumgartner <stefan.baumgartner@dynatrace.com>
2016-04-06 15:29:42 +0200

    wow samples

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,17 @@ const path         = require('path');
 const bSync        = require('browser-sync');
 
 const styleFiles = 'src/**/*.scss';
-
-const files = globby
-  .sync('src/**/README.md');
+const site = {};
+site.components = globby
+  .sync('src/**/README.md')
+  .map(el => {
+    el = el.replace('README.md', '').replace('src/', '');
+    var parts = el.split('/');
+    return {
+      url: el,
+      name: capitalizeFirstLetter(parts[parts.length - 2])
+    }
+  });
 
 gulp.task('styles:lint', () => {
   return stylelint.lint({
@@ -50,12 +58,17 @@ function buildSite() {
   return through2.obj(function(file, enc, cb) {
     let template = file.page.template || 'components';
     const templateFile = path.join(__dirname, 'docs', '_templates', `${template}.html`);
-    const els = file.path.split('/');
+    let els = file.path.split('/');
     const data = {
+      site,
       page: file.page,
       title: capitalizeFirstLetter(els[els.length - 2]),
       content: file.contents.toString()
     };
+
+    els.pop();
+    data.samples = globby.sync(els.join('/') + '/**/*.html');
+
     const tpl = swig.compileFile(templateFile, {cache: false});
     file.contents = new Buffer(tpl(data));
     cb(null, file);
