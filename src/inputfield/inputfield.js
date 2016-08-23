0d269420c799d05fd9431f05d11c93fc7e93d98a
Freihofner, Katrin <Katrin.Freihofner@ruxit.com>
2016-08-23 11:13:33 +0200

    updated coloring of date input via js

--- src/inputfield/inputfield.js
+++ src/inputfield/inputfield.js
@@ -18,3 +18,27 @@ const initSearchFields = () => {
 };
 
 initSearchFields();
+
+
+function dateFieldChangeHandler() {
+  if (isNaN(this.value)) {
+    this.classList.remove('inputfield--date--placeholder');
+    this.classList.add('inputfield--date');
+  } else {
+    this.classList.remove('inputfield--date');
+    this.classList.add('inputfield--date--placeholder');
+  }
+}
+
+
+const validateDate = () => {
+  $('input[type=date]').forEach(el => {
+    if (!el.getAttribute('data-initialized')) {
+      el.addEventListener('keyup', dateFieldChangeHandler);
+      el.setAttribute('data-initialized', true);
+    }
+  });
+};
+
+
+validateDate();
