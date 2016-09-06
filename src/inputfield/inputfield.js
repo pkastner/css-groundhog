bb390b6f52605682b31aa2fd8d7727e65e46cbce
Freihofner, Katrin <Katrin.Freihofner@ruxit.com>
2016-09-06 12:52:54 +0200

    fixed search icon, added aria-described by, moved label to commons

--- src/inputfield/inputfield.js
+++ src/inputfield/inputfield.js
@@ -1,24 +1,5 @@
 import $ from '../js-common-components/dollar';
 
-function searchFieldChangeHandler() {
-  if (this.value !== '') {
-    this.previousElementSibling.classList.remove('inputfield__icon--search');
-  } else {
-    this.previousElementSibling.classList.add('inputfield__icon--search');
-  }
-}
-
-const initSearchFields = () => {
-  $('input[type=search]').forEach(el => {
-    if (!el.getAttribute('data-initialized')) {
-      el.addEventListener('keyup', searchFieldChangeHandler);
-      el.setAttribute('data-initialized', true);
-    }
-  });
-};
-
-initSearchFields();
-
 
 function dateFieldChangeHandler() {
   if (isNaN(this.value)) {
