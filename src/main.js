d6451a94bd0a52047c7fa07f238080c4be734558
Thomas Heller <thomas.heller@dynatrace.com>
2016-09-15 15:23:16 +0200

    feat(tabs): script to enable tab toggling

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ require('./js-common-components/toggler');
 require('./js-common-components/closest');
 require('./expandable/expandable');
 require('./inputfield/inputfield');
+require('./tabs/tabs');
