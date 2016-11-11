992174b6dcfd331771ee5370e63f2ce98948a8e1
Freihofner, Katrin <Katrin.Freihofner@ruxit.com>
2016-11-11 12:42:00 +0100

    fix(slider): set min max if they are null

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -1,10 +1,13 @@
 import $ from '../js-common-components/dollar';
 
 function colorRangeInput() {
+  this.min = this.min || 0;
+  this.max = this.max || 100;
+
   const currentValue = ((this.value - this.min) / (this.max - this.min)) * 100;
   this.style.backgroundImage =
- `linear-gradient(to right, #00a1b2 0%, #00a1b2 ${currentValue}%,
-#ccc ${currentValue}%, #ccc 100%)`;
+  `linear-gradient(to right, #00a1b2 0%, #00a1b2 ${currentValue}%,
+  #ccc ${currentValue}%, #ccc 100%)`;
 }
 
 const init = () => {
