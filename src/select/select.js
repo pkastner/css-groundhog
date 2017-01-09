6d08a1c712f9c4d604c452686d6506631b2550d7
Freihofner, Katrin <Katrin.Freihofner@ruxit.com>
2017-01-09 16:46:21 +0100

    fix(select): updated js and html to have options directly in the html and not loaded from a json

--- src/select/select.js
+++ src/select/select.js
@@ -1,26 +1,4 @@
 import $ from '../js-common-components/dollar';
-require('../js-common-components/accordion');
-
-function fetchData(select) {
-  const rq = { url: '/assets/data/select_data.json',
-    onLoad: (e) => {
-      const options = (JSON.parse(e.target.responseText)).options;
-      for (let i = 0; i < options.length; i++) {
-        const opt = document.createElement('option');
-        opt.value = options[i].value;
-        opt.innerHTML = options[i].value;
-        select.add(opt);
-      }
-    },
-  };
-
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', rq.onLoad.bind(this));
-
-  xhr.open('GET', rq.url);
-  xhr.send();
-}
-
 
 const initData = () => {
   $('select').forEach(el => {
@@ -35,7 +13,6 @@ const initData = () => {
         ev.stopPropagation();
         el.classList.remove('is-expanded');
       });
-      fetchData(el);
       el.setAttribute('data-initialized', true);
     }
   });
