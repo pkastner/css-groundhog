d7a9c0a84c915cad0c27b7c883af20e9b27d3d42
Freihofner, Katrin <Katrin.Freihofner@ruxit.com>
2017-01-26 12:39:51 +0100

    feat(table): js for responsive table

--- src/table/table.js
+++ src/table/table.js
@@ -1,22 +1,29 @@
 import $ from '../js-common-components/dollar';
 
-const init = () => {
-  const i = ($('th').length);
+const fillData = (table) => {
+  const th = table.querySelectorAll('th');
+  const i = th.length;
   const header = [];
-  $('th').forEach(el => {
+  th.forEach(el => {
     header.push(el.textContent);
-    if (!el.getAttribute('data-initialized')) {
-      el.setAttribute('data-initialized', true);
-    }
   });
   let j = i - 1;
-  $('td').forEach(cell => {
+  const td = table.querySelectorAll('td');
+  td.forEach(cell => {
     cell.setAttribute('data-th', header[i - 1 - j]);
     if (j > 0) {
       j--;
     } else {
-      console.log(j);
       j = i - 1;
+    }
+  });
+};
+
+const init = () => {
+  $('table').forEach(table => {
+    if (!table.getAttribute('data-initialized')) {
+      table.setAttribute('data-initialized', true);
+      fillData(table);
     }
   });
 };
